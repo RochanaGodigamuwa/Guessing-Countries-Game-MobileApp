77b59c1bdfe223110922d8c2b018b8f1d05c34e7
RochanaGodigamuwa <125296645+RochanaGodigamuwa@users.noreply.github.com>
2024-04-06 08:45:17 +0530

    First Button Flag takens randomly done, modifications on going

--- app/src/main/java/com/example/cw1/MainActivity.kt
+++ app/src/main/java/com/example/cw1/MainActivity.kt
@@ -10,13 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cw1.ui.theme.CW1Theme
